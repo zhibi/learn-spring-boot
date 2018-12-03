661fc9a96227d97485ab623a6dcde22cf3e17e76
Dream笔 <805073259@qq.com>
2018-12-03 18:31:48 +0800

    两种国际化配置

--- spring-boot-hibernate-validator/src/main/java/zhibi/study/config/ValidatorConfig.java
+++ spring-boot-hibernate-validator/src/main/java/zhibi/study/config/ValidatorConfig.java
@@ -2,7 +2,7 @@ package zhibi.study.config;
 
 import org.hibernate.validator.HibernateValidator;
 import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
-import org.hibernate.validator.resourceloading.AggregateResourceBundleLocator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import java.util.Arrays;
 
 /**
  * @author Dream笔
@@ -29,9 +28,20 @@ public class ValidatorConfig {
         ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
                 .configure()
                 .failFast(true)
+
+                /*
+                一个配置文件的时候
+                 */
                 .messageInterpolator(new ResourceBundleMessageInterpolator(
-                        new AggregateResourceBundleLocator(Arrays.asList("i18n/error"))))
+                        new PlatformResourceBundleLocator("i18n/error")))
+
+                /*
+                多个配置文件的时候
+                 */
+                /* .messageInterpolator(new ResourceBundleMessageInterpolator(
+                         new AggregateResourceBundleLocator(Arrays.asList("i18n/error","i18n/message"))))*/
                 .buildValidatorFactory();
         return validatorFactory.getValidator();
     }
+
 }
