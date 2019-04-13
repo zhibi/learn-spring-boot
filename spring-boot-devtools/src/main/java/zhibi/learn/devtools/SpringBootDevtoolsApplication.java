b94ecc438d5dfdfa4382ba6f4aadcb02404840b3
执笔 <805073259@qq.com>
2019-04-13 11:59:47 +0800

    热加载

---
+++ spring-boot-devtools/src/main/java/zhibi/learn/devtools/SpringBootDevtoolsApplication.java
@@ -0,0 +1,19 @@
+package zhibi.learn.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author 执笔
+ * @date 2019/4/13 11:07
+ */
+@SpringBootApplication
+public class SpringBootDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsApplication.class, args);
+    }
+
+}
