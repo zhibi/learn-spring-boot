4df715142faf253f9bab34eb604110bcf37f0ac2
执笔 <805073259@qq.com>
2019-07-10 18:04:15 +0800

    :bento: 项目打包并启动

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## 笔记文档
 > https://www.yuque.com/zhibi./springboot
+
+## spring-boot-base
+### 项目打包并启动
+> https://www.yuque.com/.zhibi/springboot/cuzzc8
--- spring-boot-base/src/main/java/zhibi/learn/springbootbase/BaseApplication.java
+++ spring-boot-base/src/main/java/zhibi/learn/springbootbase/BaseApplication.java
@@ -1,12 +1,18 @@
-package zhibi.learn;
+package zhibi.learn.springbootbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author 执笔
+ * @date 2019/7/10 17:43
+ */
 @SpringBootApplication
-public class SpringBootCacheApplication {
+public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootCacheApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
+
 }
